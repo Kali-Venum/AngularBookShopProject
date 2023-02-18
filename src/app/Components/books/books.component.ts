3aa3c584cc025caa5718d9ca63846fc7727cc9af
Kali-Venum <subhamdas.abcd@gmail.com>
2023-02-18 20:12:44 +0530

    books & book component created.

---
+++ src/app/Components/books/books.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Book } from 'src/app/Models/Book.model';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css'],
+})
+export class BooksComponent {
+  books: Book[] = [
+    {
+      image: 'https://m.media-amazon.com/images/I/51Do0NLYmDL._SX258_BO1,204,203,200_.jpg',
+      name: "World's Greatest Books For Personal Growth & Wealth",
+      author: 'Dale Carnegie',
+      price: 100
+    },
+    {
+      image: 'https://m.media-amazon.com/images/I/41RwQlo446L._SX329_BO1,204,203,200_.jpg',
+      name: "Energize Your Mind",
+      author: 'Gaur Gopal Das',
+      price: 200
+    },
+  ];
+
+  isShowing: boolean = true;
+}
